9b0642f4667f29b347c440d00e169d6eef968a28
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-04-19 16:49:21 -0700

    Until lesson 50.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,13 +1,63 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { 
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
   @Input('img') postImg = '';
-  test: string = 'This is a relationship from child to parent'; 
-
   @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('constructor() called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit() called')
+  }
+
+  ngOnChanges(): void {
+    console.log("ngOnChanges() called");
+    
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck() called");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit(): called");
+    
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked(): called");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit(): called");
+
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewInit(): called");
+
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy() called")
+  }
+
 }
