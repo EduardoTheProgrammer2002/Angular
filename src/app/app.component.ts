94379ec85d6115795f979ccb686681a423e0728c
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-04-18 17:02:01 -0700

    Basics changes, start up of the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<h1>Hello World</h1>',
+  templateUrl: 'app.component.html',
+  styleUrls: [
+    'app.component.css'
+  ]
+  
 })
 export class AppComponent {
-  title = 'basics';
+  name: string = "Eduardo"
+  imgUrl: string = 'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+  
+  getName(): string {
+    return this.name;
+  }
 }
